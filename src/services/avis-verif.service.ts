97a4f5fd8eaa6cba5d1f7874389f0131f92cb5a7
0xb4lamx <medghaith.sellami@gmail.com>
2019-11-20 09:40:26 +0100

    refactor(avis-verif.service): return promise

--- src/services/avis-verif.service.ts
+++ src/services/avis-verif.service.ts
@@ -1,8 +1,12 @@
 import { HttpService, Inject, Injectable } from '@nestjs/common';
-import { AV_VERIF_CONNECT_OPTIONS } from '../constants';
-import { IAvisVerifConfigOptions, IAvisVerifServiceOrderParams } from '..';
-import * as SHA1 from 'sha1';
 import * as querystring from 'querystring';
+import { map } from 'rxjs/operators';
+import * as SHA1 from 'sha1';
+
+import { AV_VERIF_CONNECT_OPTIONS } from '../constants';
+
+import { IAvisVerifConfigOptions, IAvisVerifServiceOrderParams } from '..';
+
 import { AvVerifLogger } from './avis-verif-logger.service';
 
 @Injectable()
@@ -27,7 +31,7 @@ export class AvisVerifService {
     /**
      * @param  {IAvisVerifServiceOrderParams} configParams
      */
-    send(configParams: IAvisVerifServiceOrderParams) {
+    async send(configParams: IAvisVerifServiceOrderParams): Promise<any> {
         this._sign = SHA1(
             configParams.query +
             configParams.order_ref +
@@ -39,23 +43,19 @@ export class AvisVerifService {
             this._secureKey,
         );
 
-        const obj = {
-            sign: this._sign,
-            ...configParams,
-        };
+        const obj = { sign: this._sign, ...configParams };
 
-        const finalObj = {
-            idWebsite: this._idWebsite,
-            message: JSON.stringify(obj),
-        };
+        const finalObj = { idWebsite: this._idWebsite, message: JSON.stringify(obj) };
 
         const headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
         };
         return this._http.post(
-            this._urlAv + '?action=act_api_notification_sha1&type=json2',
+            `${this._urlAv}?action=act_api_notification_sha1&type=json2`,
             querystring.stringify(finalObj),
             { headers },
-        );
+            )
+            .pipe(map((response) => response.data))
+            .toPromise();
     }
 }
